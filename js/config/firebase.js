06ffcefc6ad2e5a451024227f79cb2ddec8ee0d3
Matvey Andrienko <m.y.andrienko@outlook.com>
2017-03-08 03:58:39 +0300

    Install firebase and add firebase config

---
+++ js/config/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBbG790Aud0U5tNYQxiXFkwCGXWr3Lhi14",
+    authDomain: "emplus-62e8f.firebaseapp.com",
+    databaseURL: "https://emplus-62e8f.firebaseio.com",
+    storageBucket: "emplus-62e8f.appspot.com",
+    messagingSenderId: "328873256963"
+};
+
+export default firebaseConfig;
