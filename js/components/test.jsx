e4cb1a5b141f055a1a195d859b11903fa7f82c2b
Matvey Andrienko <m.y.andrienko@outlook.com>
2017-03-08 03:59:05 +0300

    Bind TestComponent to firebase reference

--- js/components/test.jsx
+++ js/components/test.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import firebase from 'firebase';
 
 export default class TestComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            testValue: ''
+        };
+    }
+
+    componentWillMount() {
+        this.firebaseRef = firebase.database().ref('/test');
+        this.firebaseRef.on('value', function (data) {
+            this.setState({
+                testValue: data.val()
+            });
+        }.bind(this));
+    }
+
+    componentWillUnmount() {
+        this.firebaseRef.off();
+    }
+    
     render() {
         return (
-            <h1>Hello, world!</h1>
+            <h1>Value: '{ this.state.testValue }'</h1>
         );
     }
 }
--- js/main.jsx
+++ js/main.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
+import firebaseConfig from './config/firebase';
 import TestComponent from './components/test.jsx';
+
+firebase.initializeApp(firebaseConfig);
+firebase.auth().signInWithEmailAndPassword('test@test.com', 'password');
 
 ReactDOM.render(<TestComponent />, document.getElementById('root'));
